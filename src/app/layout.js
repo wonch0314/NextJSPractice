0ac8baa94dbf9cb8f6c9583050829de3e7899f7b
원찬호 <chanho.won@kt.com>
2024-01-03 17:58:34 +0900

    temp

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>{children}</body>
+      <body>
+        <h1>
+          <a href="/">WEB</a>
+        </h1>
+        <ol>
+          <li>
+            <a href="/read/1">html</a>
+          </li>
+          <li>
+            <a href="/read/2">css</a>
+          </li>
+        </ol>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <>Hello, NextJS</>;
+  return (
+    <>
+      <h2>Welcome</h2>
+      Hello, WEb
+    </>
+  );
 }
